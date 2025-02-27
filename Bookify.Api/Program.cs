68f74719f93e5cb5b4b9c94711cadab464a51cca
SliesarenkoDima <slesarenkodima1996@gmail.com>
2025-02-27 20:40:48 +0200

    Added presentation layer, added docker compose

---
+++ Bookify.Api/Program.cs
@@ -0,0 +1,23 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
