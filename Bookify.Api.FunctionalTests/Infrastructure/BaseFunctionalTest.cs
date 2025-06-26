e5a5aac039d20b792be80cb197e83e5548631fff
SliesarenkoDima <slesarenkodima1996@gmail.com>
2025-06-26 20:20:23 +0300

    Added functional tests, fixed validation

---
+++ Bookify.Api.FunctionalTests/Infrastructure/BaseFunctionalTest.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Api.Controllers.Users;
+using Bookify.Api.FunctionalTests.Users;
+using Bookify.Application.Users.LogInUser;
+using System.Net.Http.Json;
+
+namespace Bookify.Api.FunctionalTests.Infrastructure;
+
+public abstract class BaseFunctionalTest : IClassFixture<FunctionalTestWebAppFactory>
+{
+    protected readonly HttpClient httpClient;
+
+    protected BaseFunctionalTest(FunctionalTestWebAppFactory factory)
+    {
+        httpClient = factory.CreateClient();
+    }
+
+    protected async Task<string> GetAccessToken()
+    {
+        HttpResponseMessage loginResponse = await httpClient.PostAsJsonAsync("api/v1/users/login", new LogInUserRequest(UserData.RegisterTestUserRequest.Email, UserData.RegisterTestUserRequest.Password));
+
+        var accessTokenResponse = await loginResponse.Content.ReadFromJsonAsync<AccessTokenResponse>();
+
+        return accessTokenResponse!.AccessToken;
+    }
+}
---
+++ Bookify.Api.FunctionalTests/Users/RegisterUserTests.cs
@@ -0,0 +1,43 @@
+﻿using Bookify.Api.Controllers.Users;
+using Bookify.Api.FunctionalTests.Infrastructure;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Bookify.Api.FunctionalTests.Users;
+
+public class RegisterUserTests : BaseFunctionalTest
+{
+    public RegisterUserTests(FunctionalTestWebAppFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Register_ShouldReturnOk_WhenRequestIsValid()
+    {
+        // Arrange
+        var request = new RegisterUserRequest("create@test.com", "first", "last", "12345");
+
+        // Act
+        var response = await httpClient.PostAsJsonAsync("api/v1/users/register", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Theory]
+    [InlineData("", "first", "last", "12345")]
+    [InlineData("test.com", "first", "last", "12345")]
+    [InlineData("@test.com", "first", "last", "12345")]
+    public async Task Register_ShouldReturnBadRequest_WhenRequestIsInvalid(string email, string firstName, string lastName, string password)
+    {
+        // Arrange
+        var request = new RegisterUserRequest(email, firstName, lastName, password);
+
+        // Act
+        var response = await httpClient.PostAsJsonAsync("api/v1/users/register", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+}
